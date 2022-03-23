fa14308a31eb3b1219c8a24841537d95e360ac46
Boya <boyazhang123@gmail.com>
2022-03-22 19:04:21 -0700

    change classnames

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,28 +1,28 @@
 const Hero = () => {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
           <img
-            class="object-cover object-center rounded"
+            className="object-cover object-center rounded"
             alt="hero"
             src="https://dummyimage.com/720x600"
           />
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Baddie Lecture Summarizer
-            <br class="hidden lg:inline-block" />
+            <br className="hidden lg:inline-block" />
           </h1>
-          <p class="mb-8 leading-relaxed">
-            Summarizes your lectures so you have more time to spend chasing
-            baddies.
+          <p className="mb-8 leading-relaxed">
+            Summarizes your lectures so you have more time to spend with baddies
+            😈
           </p>
-          <div class="flex justify-center">
-            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Upload
             </button>
-            <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
               Confirm
             </button>
           </div>
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <header class="text-gray-600 body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -10,28 +10,28 @@ const Navbar = () => {
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span class="ml-3 text-xl">Baddie Summarizer</span>
+          <span className="ml-3 text-xl">Baddie Summarizer</span>
         </a>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900" href="/">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900" href="/">
             Home
           </a>
-          <a class="mr-5 hover:text-gray-900" href="/how">
+          <a className="mr-5 hover:text-gray-900" href="/how">
             How it works
           </a>
-          <a class="mr-5 hover:text-gray-900" href="/testimonials">
+          <a className="mr-5 hover:text-gray-900" href="/testimonials">
             Testimonials
           </a>
-          <a class="mr-5 hover:text-gray-900" href="/dashboard">
+          <a className="mr-5 hover:text-gray-900" href="/dashboard">
             Dashboard
           </a>
         </nav>
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Try it now
           <svg
             fill="none"
@@ -39,7 +39,7 @@ const Navbar = () => {
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            class="w-4 h-4 ml-1"
+            className="w-4 h-4 ml-1"
             viewBox="0 0 24 24"
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
