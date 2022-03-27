474229e7aa306be707972e7e9de88595174cc214
Ernest Wong <ernestwong1027@gmail.com>
2022-03-27 13:43:40 -0400

    fixed upload/donwload

--- frontend/components/FileUploader.js
+++ frontend/components/FileUploader.js
@@ -1,99 +1,98 @@
-import React, { useRef, useState } from 'react';
+import React, {useRef, useState} from 'react';
 // Style the Button component
 
 const FileUploader = (props) => {
-  // Create a reference to the hidden file input element
-  const hiddenFileInput = useRef(null);
-  const downloadButton = useRef(null);
+    // Create a reference to the hidden file input element
+    const hiddenFileInput = useRef(null);
+    const downloadButton = useRef(null);
 
-  const [loading, setLoading] = useState(false);
-  const [downloadHref, setDownloadHref] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [downloadHref, setDownloadHref] = useState('');
 
-  // Programatically click the hidden file input element
-  // when the Button component is clicked
-  const handleClick = (event) => {
-    hiddenFileInput.current.click();
-  };
+    // Programatically click the hidden file input element
+    // when the Button component is clicked
+    const handleClick = (event) => {
+        hiddenFileInput.current.click();
+    };
 
-  const handleChange = async (event) => {
-    try {
-      setLoading(true);
-      const body = {
-        file: event.target.files[0],
-      };
+    const handleChange = async (event) => {
+        try {
+            setLoading(true);
+            let formData = new FormData();
+            formData.append('file', event.target.files[0])
 
-      const result1 = await fetch('https://ernestw.com:5000/submit', {
-        method: 'POST',
-        body,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      });
 
-      const data = await result1.json();
+            const result1 = await fetch('http://localhost:5000/submit', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            });
 
-      if (data.filename) {
-        const result2 = await fetch(
-          `https://ernestw.com:5000/file/${data.filename}`
-        );
+            const data = await result1.json();
+            let filename = data.filename.split('.')[0]
 
-        if (result2.statusCode === 200) {
-          // todo: get specific attribute of data
-          const url = window.URL.createObjectURL(data);
-          setDownloadHref(url);
+            const result2 = await fetch(
+                `http://localhost:5000/file/${filename}`
+            );
+
+            let file = await result2.blob()
+            const url = window.URL.createObjectURL(file);
+            console.log(url)
+            setDownloadHref(url);
+
+            setLoading(false);
+        } catch (err) {
+            console.log(err);
         }
-      }
-      setLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return (
-    <>
-      <button
-        type="submit"
-        onClick={handleClick}
-        className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        {!loading ? (
-          'Upload a file'
-        ) : (
-          <div className="flex flex-row items-center">
-            <svg
-              role="status"
-              class="mr-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+    };
+    return (
+        <>
+            <button
+                type="submit"
+                onClick={handleClick}
+                className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <p>Processing...</p>
-          </div>
-        )}
-      </button>
-      <input
-        type="file"
-        ref={hiddenFileInput}
-        onChange={handleChange}
-        className="hidden"
-      />
-      <button
-        className="inline-flex text-white ml-32 bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-        disabled={downloadHref}
-      >
-        <a href={downloadHref} ref={downloadButton} download>
-          Download
-        </a>
-      </button>
-    </>
-  );
+                {!loading ? (
+                    'Upload a file'
+                ) : (
+                    <div className="flex flex-row items-center">
+                        <svg
+                            role="status"
+                            class="mr-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                            />
+                        </svg>
+                        <p>Processing...</p>
+                    </div>
+                )}
+            </button>
+            <input
+                type="file"
+                ref={hiddenFileInput}
+                onChange={handleChange}
+                className="hidden"
+            />
+            <button
+                className="inline-flex text-white ml-32 bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                disabled={downloadHref}
+            >
+                <a href={downloadHref} ref={downloadButton} download>
+                    Download
+                </a>
+            </button>
+        </>
+    );
 };
 export default FileUploader;
