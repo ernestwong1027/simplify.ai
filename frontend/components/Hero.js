0588616ad65c615597c0bf1c13bd5d92673478af
Boya <boyazhang123@gmail.com>
2022-03-27 11:13:28 -0700

    change homepage text

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -14,8 +14,8 @@ const Hero = () => {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            <br className="hidden lg:inline-block" />
-            Simplify.ai Lecture Summarizer
+            <br className="hidden lg:inline-block" />A free, open source lecture
+            summarizer
           </h1>
           <p className="mb-8 leading-relaxed">
             Simply drag and drop your audio file or find it from your computer,
