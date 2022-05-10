82732656b3aaf0e170ed4da279a4ceedbb0172d0
jedideen2 <100820265+jedideen2@users.noreply.github.com>
2022-05-09 22:34:46 -0400

    Update main.py

--- model class sets/main.py
+++ model class sets/main.py
@@ -4,16 +4,17 @@ from audioenhancement import audioenhancement
 from pegasustrainer import PegasusTraining
 from formattext import FormatText
 
-device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+if __name__ == "main":
+    device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
-audiofile = glob('CREATEPATHTOFILE')
-loaded_audio_enhancement = audioenhancement(audiofile)
-enhanced_audio = loaded_audio_enhancement.enhance()
-loaded_speech_to_text = SpeechToText(text_files=enhanced_audio)
-output_text = enhanced_audio.convert()
-loaded_formatted_text = FormatText(input_text=output_text, output='')
-final_text = loaded_formatted_text.format()
-loaded_summarization = Pegasus(final_text)
-summarized_text = loaded_summarization.summarize()
+    audiofile = glob('CREATEPATHTOFILE')
+    loaded_audio_enhancement = audioenhancement(audiofile)
+    enhanced_audio = loaded_audio_enhancement.enhance()
+    loaded_speech_to_text = SpeechToText(text_files=enhanced_audio)
+    output_text = enhanced_audio.convert()
+    loaded_formatted_text = FormatText(input_text=output_text, output='')
+    final_text = loaded_formatted_text.format()
+    loaded_summarization = Pegasus(final_text)
+    summarized_text = loaded_summarization.summarize()
 
-return summarized_text
+    return summarized_text
